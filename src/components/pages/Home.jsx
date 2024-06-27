bfa9e6c44d48cb5bc6a8058a9b94960d830695ff
Nandasyahputra224 <nsyahputra224@gmail.com>
2024-06-27 10:59:11 +0700

    add image to public

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="about font-mont flex justify-center items-center text-4xl font-black py-80">
+    <div className="about flex justify-center items-center text-5xl font-bold py-80">
       Coming soon...
     </div>
   );
